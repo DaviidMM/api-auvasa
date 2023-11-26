9ac97dfb83c1b5855cfdbff4b531a4178cecf3f5
DaviidMM <davicitoo1612@gmail.com>
2023-11-26 15:41:42 +0100

    Add custom swagger css

--- routes/v1/swagger/index.js
+++ routes/v1/swagger/index.js
@@ -26,7 +26,11 @@ const swaggerDocs = (route) => {
   route.use(
     '/docs',
     swaggerUi.serve,
-    swaggerUi.setup(swaggerSpec, { customCssUrl: CSS_URL }),
+    swaggerUi.setup(swaggerSpec, {
+      customCss:
+        '.swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }',
+      customCssUrl: CSS_URL,
+    }),
   );
   // Make our docs in JSON format available
   route.get('/docs.json', (req, res) => {
