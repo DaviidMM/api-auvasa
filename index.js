f74144b1a91313e9c4a6862f7af7e3094a50c689
DaviidMM <davicitoo1612@gmail.com>
2023-11-28 18:41:44 +0100

    Remove commented out Swagger documentation code

--- index.js
+++ index.js
@@ -11,40 +11,6 @@ app.set('json spaces', 2);
 // Permitir todas las peticiones CORS
 app.use(cors({ origin: '*' }));
 
-// Swagger doc
-// const swaggerJSDoc = require('swagger-jsdoc');
-// const swaggerUi = require('swagger-ui-express');
-
-// // API meta info
-// const options = {
-//   definition: {
-//     openapi: '3.0.0',
-//     info: {
-//       title: 'API AUVASA',
-//       version: '1.0.0',
-//       description: 'API para obtener información de la web de AUVASA',
-//     },
-//   },
-//   apis: ['routes/**/*.js'],
-// };
-
-// // Docs in JSON format
-// const swaggerSpec = swaggerJSDoc(options);
-
-// const CSS_URL =
-//   'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.6.2/swagger-ui.min.css';
-
-// app.use(
-//   '/docs',
-//   swaggerUi.serve,
-//   swaggerUi.setup(swaggerSpec, { customCssUrl: CSS_URL }),
-// );
-// // Make our docs in JSON format available
-// app.get('/docs.json', (req, res) => {
-//   res.setHeader('Content-Type', 'application/json');
-//   res.send(swaggerSpec);
-// });
-
 // Rutas
 app.use('/v1', v1Routes);
 app.use('/', defaultRoutes);
